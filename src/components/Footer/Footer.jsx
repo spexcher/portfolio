a6db0e5f15a822e2302a244d9dfb7ce7741b1cea
Gourab Modak (spexcher) <spexcher@gmail.com>
2024-04-10 00:23:40 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/components/Footer/Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
-import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-import Gitub from "@iconscout/react-unicons/icons/uil-github";
 import { SiCodechef } from "react-icons/si";
 import { SiCodeforces } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
@@ -18,7 +15,7 @@ const Footer = () => {
         <div className="footer">
             <img src={Wave} alt="" style={{ width: "100%" }} />
             <div className="f-content">
-                <span className="EMAIL">gourabmodak28092003@gmail.com</span>
+                <span className="EMAIL">spexcher@gmail.com</span>
                 <div className="f-icons">
                     {/* <Insta color="white" size={"3rem"} />
           <Facebook color="white" size={"3rem"} />
