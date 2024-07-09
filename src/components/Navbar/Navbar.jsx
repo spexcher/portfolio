f0eb240e2ccfac57ef86c7490d90caffca150f54
Gourab Modak <137685981+spexcher@users.noreply.github.com>
2024-07-09 15:51:52 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const navbar = () => {
 {/*                             <Link to="services" spy={true} smooth={true}>
                                 Serivces
                             </Link> */}
-                            <a style="text-decoration:none" href="https://app.onlinedegree.iitm.ac.in/student/23F2001573">IIT Madras Profile</a>
+                            <Link style="text-decoration:none" to="https://app.onlinedegree.iitm.ac.in/student/23F2001573">IIT Madras Profile</a>
                         </li>
                         {/* <li>
               <Link to="works" spy={true} smooth={true}>
