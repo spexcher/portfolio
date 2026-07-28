21b59001bf890c79cded02501e7a2640c5330b9a
Gourab Modak <137685981+spexcher@users.noreply.github.com>
2026-07-29 01:14:40 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -32,7 +32,7 @@ export const HeroSection = () => {
     and browser-based developer tools.
   </p>
 
-          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+          <div className="pt-4 opacity-0 gap-4 animate-fade-in-delay-4">
             <a href="#projects" className="cosmic-button">
               View My Work
             </a>
